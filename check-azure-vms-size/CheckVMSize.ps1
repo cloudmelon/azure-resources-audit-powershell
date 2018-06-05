c1508cbdb2097ca20c0d273cb9c73348f903f66c
MelonyQ <urinaway@gmail.com>
2018-06-05 10:37:07 +0200

    add new script to check vm size in current subscription

---
+++ check-azure-vms-size/CheckVMSize.ps1
@@ -0,0 +1,34 @@
+﻿Login-AzureRmAccount
+
+$subscription = "Visual Studio Enterprise"
+Set-AzureRmContext -Subscription $subscription
+
+
+################################################### List all the VMs  ###################################################
+#all the vms in the same size ( list vm but with size information ) 
+
+Write-Output ("There are "+ + (Get-AzureRmVM).length + " Virtual machines in current subscription, here they are : ")
+
+
+#Get all resource groups in current subscription 
+$rgs = Get-AzureRmResourceGroup
+
+#Current subscription :
+
+
+Write-Output ("Here are the details in subscription "+ $subscription + " each resource type in resource group : " )
+#Get all resources in resource group OK
+$rgs = Get-AzureRmResourceGroup
+
+   Write-Output "VmName,VmInResourceGroup,VmSize,NumberOfCores,MemoryInMB,MaxDataDiskCount,OSDiskSizeInMB,ResourceDiskSizeInMB"
+   Get-AzureRmVM |  ForEach {
+   $Availablevmsize = Get-AzureRmVMSize -ResourceGroupName $_.ResourceGroupName -VMName $_.Name
+   Foreach($size in $Availablevmsize){
+      If ($_.hardwareProfile.vmSize -eq $size.Name ) {  
+         Write-Output ($_.Name + "," + $_.ResourceGroupName + "," + $size.Name  + "," + $size.NumberOfCores + "," + $size.MemoryInMB  + "," + $size.MaxDataDiskCount  + "," + $size.OSDiskSizeInMB  + "," + $size.ResourceDiskSizeInMB)
+      }
+   }
+  } |Sort-Object -Property VmSize 
+
+
+
