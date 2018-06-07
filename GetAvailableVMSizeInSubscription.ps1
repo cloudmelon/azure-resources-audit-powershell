bc5a085d2c41cfef432690216260adc54d8b2737
MelonyQ <urinaway@gmail.com>
2018-06-07 10:06:30 +0200

    create azure ps to get all the available size in current subscription

---
+++ GetAvailableVMSizeInSubscription.ps1
@@ -0,0 +1,10 @@
+﻿Login-AzureRmAccount
+
+
+$subscription = "Visual Studio Entreprise"
+
+
+ Set-AzureRmContext -Subscription $subscription
+
+#All available SKU in current subscription 
+Get-AzureRmVM | select -Property @{Label="VmSize";Expression={$_.HardwareProfile.VmSize}} | sort-object -Property VmSize -Unique
