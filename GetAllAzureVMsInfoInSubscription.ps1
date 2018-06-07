ae3957987db9650aaec69f5cc4e5d1c88eb48d43
MelonyQ <urinaway@gmail.com>
2018-06-07 10:03:26 +0200

    Add script to Get All Azure VMs Info In Subscription

---
+++ GetAllAzureVMsInfoInSubscription.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRmAccount
+
+
+$subscription = "Visual Studio Entreprise"
+
+
+ Set-AzureRmContext -Subscription $subscription
+
+
+#list all the vms group by VM Size
+Get-AzureRmVM | Select -Property Name, @{Label="OsType";Expression={$_.StorageProfile.OsDisk.OsType}}, @{Label="VmSize";Expression={$_.HardwareProfile.VmSize}} |Sort-Object -Property VmSize 
