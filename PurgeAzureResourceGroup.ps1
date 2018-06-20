cfa5a9b6233b8373ff834706e902d7c17ee56959
cloud_melon <urinaway@gmail.com>
2018-06-20 12:12:01 +0200

    Delete PurgeAzureResourceGroup.ps1
    Moving to another related repository, so that this repo only use for audit resource not managing resource

--- PurgeAzureResourceGroup.ps1
+++
@@ -1,3 +0,0 @@
-﻿# Purge a resource group 
-
-New-AzureRmResourceGroupDeployment -ResourceGroupName testvmssnewfeatureone -Mode Complete -TemplateFile .\resource\ResourceGroupPurge.template.json -Force -Verbose
